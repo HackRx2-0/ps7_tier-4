2bf865b660a205de3c183508bfe29e9158933352
aaryan11-hash <aaryan2000dpspune@gmail.com>
2021-07-23 12:41:04 +0530

    chore(android):Implemented utility functions for base64 string to mat object and vice-versa

--- android/app/src/main/java/com/reactlibrary/RNOpenCvLibraryModule.java
+++ android/app/src/main/java/com/reactlibrary/RNOpenCvLibraryModule.java
@@ -15,6 +15,37 @@ public class RNOpenCvLibraryModule extends ReactContextBaseJavaModule{
       return "RNOpenCvLibrary";
     }
 
+    public Mat base64ToMat(String imageAsBase64){
+
+      byte[] decodedString = Base64.decode(imageAsBase64, Base64.DEFAULT);
+
+      Bitmap selectedImage = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+      
+      Mat srcOrig = new Mat(selectedImage.getHeight(), selectedImage.getWidth(), CvType.CV_8UC4);
+      
+      Utils.bitmapToMat(selectedImage, srcOrig);
+
+      return srcOrig;
+  }
+
+
+  public String matTobase64(Mat src) {
+
+      Bitmap finalImage = Bitmap.createBitmap(src.cols(), src.rows(), Bitmap.Config.ARGB_8888);
+
+      Utils.matToBitmap(src, finalImage);
+      
+      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+      finalImage.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+
+      byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+      String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+      return encoded;
+  }
+
 
     @ReactMethod
     public void addContrastMethod(String imageAsBase64, int alpha ,Callback errorCallback,
@@ -26,6 +57,8 @@ public class RNOpenCvLibraryModule extends ReactContextBaseJavaModule{
     public void rotate90Degrees(String imageAsBase64, Callback errorCallback, Callback successCallback){
         
     }
+
+
   
 }
 
