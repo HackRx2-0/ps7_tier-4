e8e3cde9727bb10422a6efe3648d9bd0f9641a28
aaryan11-hash <aaryan2000dpspune@gmail.com>
2021-07-23 11:57:57 +0530

    chore(android):configuration of java native module,initialised addcontrast method

--- android/app/src/main/java/com/reactlibrary/RNOpenCvLibraryModule.java
+++ android/app/src/main/java/com/reactlibrary/RNOpenCvLibraryModule.java
@@ -1,6 +1,26 @@
 package com.reactlibrary;
 
-public class RNOpenCvLibraryModule{
+public class RNOpenCvLibraryModule extends ReactContextBaseJavaModule{
+
+    private final ReactApplicationContext reactContext;
+    private String TAG="APP";
+  
+    public RNOpenCvLibraryModule(ReactApplicationContext reactContext) {
+      super(reactContext);
+      this.reactContext = reactContext;
+    }
+  
+    @Override
+    public String getName() {
+      return "RNOpenCvLibrary";
+    }
+
+
+    @ReactMethod
+    public void addContrastMethod(String imageAsBase64, int alpha ,Callback errorCallback,
+    Callback successCallback){
+
+    }
   
 }
 
