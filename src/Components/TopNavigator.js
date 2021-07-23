d98d6a586b93438e8022ae73488e0154f30a80d7
sanket shevkar <sanket@sankets-MacBook-Air.local>
2021-07-24 00:33:37 +0530

    ui(*): homescreen overhaul

---
+++ src/Components/TopNavigator.js
@@ -0,0 +1,14 @@
+/*eslint-disable*/
+import React from 'react';
+import {AppRegistry, View, Text} from 'react-native';
+export default function TopNavigator () {
+      return (
+          <View style={{flexDirection: 'row', backgroundColor: "#841584"}}>
+              <Text style={{ fontSize: 30, paddingLeft: 5 }}>
+              &lt;
+              </Text>
+          </View>
+      );
+};
+
+AppRegistry.registerComponent("TopNavigator", () => TopNavigator);
